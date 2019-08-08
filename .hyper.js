f25d0f76e7b2a246ae0791a4165d54f5580ae633
zakallen <hi@zak.dev>
2019-08-08 11:31:46 -0700

    commit the hypercwd plugin

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-snazzy"],
+  plugins: [
+    "hyper-snazzy",
+    "hypercwd",
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
